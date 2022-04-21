461bb121594a1bf617b20da59bb646b333b662ab
Mayoor <mayoordoiphode@gmail.com>
2022-04-21 18:33:18 +0530

    Added: Welcome message program

---
+++ Main.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.assignment9.AddressBook;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Management System..!");
+    }
+}

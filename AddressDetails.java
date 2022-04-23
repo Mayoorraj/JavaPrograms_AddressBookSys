6b60a4f2438d5a7e9bbd4af13e94f693517abb74
Mayoor <mayoordoiphode@gmail.com>
2022-04-23 23:48:32 +0530

    Added: ability to add a contact- details will be called from here

--- AddressDetails.java
+++ AddressDetails.java
@@ -24,55 +24,42 @@ public class AddressDetails {
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getPhoneNumber() {
         return phoneNumber;
     }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getCity() {
         return city;
     }
-
     public void setCity(String city) {
         this.city = city;
     }
-
     public String getState() {
         return state;
     }
-
     public void setState(String state) {
         this.state = state;
     }
-
     public String getPincode() {
         return pincode;
     }
-
     public void setPincode(String pincode) {
         this.pincode = pincode;
     }
@@ -87,6 +74,6 @@ public class AddressDetails {
                 "\nEmail: " + email +
                 "\nCity: " + city  +
                 "\nState: " + state +
-                "\nPincode: " + pincode ;
+                "\nPin code: " + pincode ;
     }
 }

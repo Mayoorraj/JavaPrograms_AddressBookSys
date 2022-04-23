3519f0e25484269236382e5034dcfea371191686
Mayoor <mayoordoiphode@gmail.com>
2022-04-23 23:47:48 +0530

    Added: ability to add a contact inan address book

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ package com.bridgelabz.assignment9.AddressBook;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Management System..!");
+        AddressFunctions contact = new AddressFunctions();
+        contact.addContact();
     }
 }

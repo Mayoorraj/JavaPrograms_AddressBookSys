eb27a09d2770833b70511ba34eece0ec88b7ced9
Mayoor <mayoordoiphode@gmail.com>
2022-04-23 10:17:02 +0530

    Added: Address details will be stored here

---
+++ AddressDetails.java
@@ -0,0 +1,91 @@
+package com.bridgelabz.assignment9.AddressBook;
+
+public class AddressDetails {
+// defining the variables
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String city;
+    private String state;
+    private String pincode;
+
+    public AddressDetails() {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.city = city;
+        this.state = state;
+        this.pincode = pincode;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+    @Override
+    public String toString() {
+        return "AddressDetails{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", pincode='" + pincode + '\'' +
+                '}';
+    }
+
+
+}

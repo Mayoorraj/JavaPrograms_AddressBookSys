ce2b157f3fd2f56dde644014be71ff4bd2b54016
Mayoor <mayoordoiphode@gmail.com>
2022-04-24 00:37:50 +0530

    Added: ability to edit a contact from an address book

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 package com.bridgelabz.assignment9.AddressBook;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Management System..!");
         AddressFunctions contact = new AddressFunctions();
         contact.addContact();
+        System.out.println("Do you want to edit the contacts ? \nYes/No");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        if (input.equals("yes") || input.equals("Yes")|| input.equalsIgnoreCase("Y")) {
+            contact.editContact();
+        } else {
+            System.out.println("Thank you..! ");
+        }
     }
 }
